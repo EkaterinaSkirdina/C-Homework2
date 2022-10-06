a4ae0c14db76b4c205f33d5ce3210e87f890de5c
Ekaterina Skirdina <katyonok.ya17@gmail.com>
2022-10-06 18:55:55 +0300

    Task 13

---
+++ Task13/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Напишите программу, которая выводит третью цифру заданного числа, 
+или сообщает что третьей цифры нет.*/
+
+
+Console.Clear();
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+string textNumber = Convert.ToString(number);
+
+if (textNumber.Length > 2)
+{
+    Console.WriteLine("третья цифра -> " + textNumber[2]);
+}
+else
+{
+Console.WriteLine("третьей цифры нет");
+}
